af7f91ad49bef4e06e96c3f080748c756a77123a
dg2026818-cmd <dg2026818@danggok.hs.kr>
2026-05-20 11:16:03 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+Web VPython 3.2
+sphere(color=color.purple, opacity=0.3)
+sphere(color=color.blue, size=vec(1.5,1.5,1.5),opacity=0.2)
+box(color=color.orange, size=vec(0.05,0.3,0.05), pos=vec(-0.2,0,0), axis=vec(1,1,0))
+box(color=color.orange, size=vec(0.05,0.3,0.05), pos=vec(-0.2,0,0), axis=vec(1,-1,0))
